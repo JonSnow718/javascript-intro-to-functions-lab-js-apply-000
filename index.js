148aa9c864d60e69a3cb0d8bf69e00735e378fd2
enthusiastic-history-5054 <jason.calvo7@gmail.com>
2019-11-12 23:02:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ function sayHiToGrandma(string)
     return "I can\'t hear you!" 
   } else if(string.toUpperCase() === string){
     return "YES INDEED!" 
-    
-  }
+    }
     I Love you, Grandma.")
 }
